ce6dfcd0f7dbcf66b33f7ba318dc5126764d3775
nmunro92 <n.munro92@gmail.com>
2021-10-05 21:58:59 +1100

    Added game images and text. Also added getting a player name and displaying that

--- jsScissors.js
+++ jsScissors.js
@@ -1,8 +1,16 @@
 
 //Rock Paper, Scissors
+
 //Global variables
 let playerScore = 0;
 let computerScore = 0;
+
+//Get the players name
+let playerName = prompt("What's your name?");
+let nameArr = document.getElementsByClassName("name");
+for(let i=0; i<nameArr.length; i++) {
+nameArr[i].innerText = playerName 
+}
 
 //Create Function for the computer to randomly return Scissors, Paper, or Rock
 let myArray = ["rock", "paper", "scissors"];
@@ -14,28 +22,28 @@ function computerPlay() {
   while ((playerScore < 3 && computerScore < 3)) {
 
 // Create a function for the player to select rock paper scissor, playerSelection. make it so the input can be case insensitive
-    let playerSelection = prompt("Scissors, Paper, Rock? Best of 5 wins!");
+    let playerSelection = prompt("Rock, Paper or Scissors? Best of 5 wins!");
 playerSelection = playerSelection.toLowerCase();
 
 //Create a function that plays rock paper scissors with the computer and declares a winner. 
 function playRound(playerSelection) {
 
   if (playerSelection === computerSelection ) {
-       return `Tie game! You: ${playerScore}. Computer: ${computerScore}.`
+       return `Tie game! ${playerName}: ${playerScore}. Computer: ${computerScore}.`
   } else if (
   (playerSelection === "scissors" && computerSelection === "paper") ||
   (playerSelection === "rock" && computerSelection === "scissors") ||
   (playerSelection === "paper" && computerSelection === "rock")
   ) {
       ++playerScore;
-      return `You win! You: ${playerScore}. Computer: ${computerScore}`;
+      return `You win! ${playerName}: ${playerScore}. Computer: ${computerScore}`;
   } else if (
       (playerSelection === "scissors" && computerSelection === "rock") ||
       (playerSelection === "rock" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "scissors")
   ) {
       ++computerScore;
-      return `You lose! You: ${playerScore}. Computer: ${computerScore}`;
+      return `You lose! ${playerName}: ${playerScore}. Computer: ${computerScore}`;
   }
   else {
      return "Sorry, that's not an option!"
